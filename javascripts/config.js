1afbfc0ce2acefea4b82572f1c2e16324ebb476c
xiaoqiang <kllb@qq.com>
2014-02-11 17:01:14 +0800

    init

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,5 +1,5 @@
 var disqus_shortname = 'xiaoqiang';
-var hostbase = 'http://xiaoqiang.github.com';
+var hostbase = 'http://xiaoqiang.github.io';
 var githubname = 'xiaoqiang';
 var repos = 'xiaoqiang.github.com';
 var sitetitle = 'xiaoqiang';
